8fb51474f2432c929a2a146343e1c0964419d7fd
Rodrigo <rgordano@hotmail.com>
2017-07-22 15:43:21 -0300

    modulo paseo cpp

--- obligatorio/paseo.cpp
+++ obligatorio/paseo.cpp
@@ -1,11 +1,34 @@
 #include "Paseo.h"
 
-void CargarPaseo(paseo &p,int nro_p,int pto_ent,int pto_sal,int cant_p);
+void CargarPaseo(paseo &p,int nro_p,int pto_ent,int pto_sal,int cant_p)
+{
+    p.nro_paseo=nro_p;
+    p.pto_entrada=pto_ent;
+    p.pto_salida=pto_sal;
+    p.cant_puntos=cant_p;
+}
 
-int DarNumero(paseo p);
+int DarNumero(paseo p)
+{
+   return p.nro_paseo;
+}
+int DarEntrada(paseo p)
+{
+  return p.pto_entrada;
+}
+int DarSalida(paseo p)
+{
+  return p.pto_salida;
+}
+int DarCantPuntos(paseo p)
+{
+   return p.cant_puntos;
+}
+void MostrarPaseo(paseo p)
+{
+  printf("Numero paseo   : %d \n",p.nro_paseo);
+  printf("Punto entrada  : %d \n",p.pto_entrada);
+  printf("Punto salida   : %d \n",p.pto_salida);
+  printf("Cantidad puntos: %d \n",p.cant_puntos);
 
-int DarEntrada(paseo p);
-
-int DarSalida(paseo p);
-
-int DarCantPuntos(paseo p);
+}
