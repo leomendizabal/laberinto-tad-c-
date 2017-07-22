765dcde4a16341b287ffb65eb560ddb870190227
Rodrigo <rgordano@hotmail.com>
2017-07-22 15:20:08 -0300

    mdulo paseo

---
+++ obligatorio/paseo.cpp
@@ -0,0 +1,11 @@
+#include "Paseo.h"
+
+void CargarPaseo(paseo &p,int nro_p,int pto_ent,int pto_sal,int cant_p);
+
+int DarNumero(paseo p);
+
+int DarEntrada(paseo p);
+
+int DarSalida(paseo p);
+
+int DarCantPuntos(paseo p);
